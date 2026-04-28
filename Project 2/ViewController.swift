5ebfc257ebd9a5b61097a64ec8e70c9f724820d2
Hunter Trujillo <megafan.exe@gmail.com>
2026-04-28 11:23:42 -1000

    Initial Commit

---
+++ Project 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 2
+//
+//  Created by Hunter Trujillo on 4/28/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
